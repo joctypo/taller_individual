9f6f898efbb82f82f8eea1ce17bd0497fdc73a65
joctypo <oscargiraldop1999@gmail.com>
2020-03-09 19:22:38 -0500

    cuarto actualizacion

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,6 @@ package main;
 
 import processing.core.PApplet;
 import processing.core.PFont;
-import processing.core.PGraphics;
 import processing.core.PImage;
 import controlP5.*;
 
@@ -10,8 +9,6 @@ public class Main extends PApplet {
 
 	ControlP5 cp5;
 	CallbackListener cb;
-	
-	
 	
 	PImage screen1;
 	PImage screen2;
@@ -28,7 +25,10 @@ public class Main extends PApplet {
 	PImage screen13;
 	PImage screen14;
 	PImage screen15;
-	
+	int pantallas=0;
+	int contadorf;
+	int tiempo;
+	PFont font;
 	
 	boolean verificar=false;
 
@@ -41,55 +41,100 @@ public class Main extends PApplet {
 		
 	}
 
-
-	public void setup() {
-		
-
-		preload();
-		
-		
-		PFont font =  createFont ( "Raleway-Regular.ttf" , 15 );
-		
-		cp5 = new ControlP5(this);
-		 noStroke();
-		 noFill();
-		 
-		 cp5.addTextfield("usuario")
-	     .setPosition(93,250)
-	     .setSize(185,26)
-	     .setFocus(true)
-	     .setFont(font)
-	     .setColor(255)
-	     .setColorBackground(color(255,255,255,1))
-	     .setColorActive(color(255,255,255,1)) 
-	     .removeBehavior() 
-	     .setLabelVisible(false)
-	     .setCaptionLabel("")
-	     ;
-
-		
-		
-		
-
-	}
-
 	public void settings() {
 		size(360,640);
 		
 
 	}
 	
+	public void setup() {
+		
+
+		preload();
+		font =  createFont ( "Raleway-Regular.ttf" , 15 );	
+		cp5 = new ControlP5(this);
+		textoslog();
+		textosreg();
+		botones();
+		
+
+		 	
+
+	}
+
+
+	
 	public void draw() {
 		background(45);
-		image(screen3,0,0);
-		text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+		
+		switch (pantallas) {
+		case 0:
+			hidetext();
+			image(screen1,0,0);
+			contadorf = frameCount;
+			if(contadorf==30) {
+				pantallas ++;
+			}
+			System.out.println(frameCount);
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+		case 1:
+			image(screen2,0,0);
+			
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+		case 2:
+			image(screen3,0,0);
+			cp5.get(Textfield.class,"usuario").setVisible(true);
+			cp5.get(Textfield.class,"password").setVisible(true);
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+		case 3:
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+		case 4:
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+		case 5:
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+		case 6:
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+		case 7:
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+		case 8:
+			text("x: "+mouseX+"y: "+mouseY,mouseX,mouseY);
+			break;
+			
+
+	
+		}
+		
+		
+		
 		
 
 		
 	}
 		
 	
+	private void hidetext() {
+		cp5.get(Textfield.class,"usuario").setVisible(false);
+		cp5.get(Textfield.class,"password").setVisible(false);
+		cp5.get(Textfield.class,"usuario2").setVisible(false);
+		cp5.get(Textfield.class,"password2").setVisible(false);
+		cp5.get(Textfield.class,"nombre").setVisible(false);
+		cp5.get(Textfield.class,"email").setVisible(false);
+	}
+	
+	
+
+
 	public void preload() {
+		
+		//Aquí cargamos todas las imagenes que vamos a utilizar 
 		screen1= loadImage("insumos/screen1.png");
 		screen2= loadImage("insumos/screen2.png");
 		screen3= loadImage("insumos/screen3.png");
@@ -108,5 +153,102 @@ public class Main extends PApplet {
 	
 	}
 
+	public void textoslog() {
+		
+		 cp5.addTextfield("usuario")
+	     .setPosition(93,250)
+	     .setSize(185,26)
+	     .setFocus(true)
+	     .setFont(font)
+	     .setColor(255)
+	     .setColorBackground(color(255,255,255,1))
+	     .setColorActive(color(255,255,255,1)) 
+	     .removeBehavior() 
+	     .setLabelVisible(false)
+	     .setCaptionLabel("")
+	     ;
+		
+		 cp5.addTextfield("password")
+	     .setPosition(93,250)
+	     .setSize(185,26)
+	     .setFocus(true)
+	     .setFont(font)
+	     .setColor(255)
+	     .setColorBackground(color(255,255,255,1))
+	     .setColorActive(color(255,255,255,1)) 
+	     .removeBehavior() 
+	     .setLabelVisible(false)
+	     .setCaptionLabel("")
+	     ;
+		 
+		
+	}
+	public void textosreg() {
+		
+		 cp5.addTextfield("usuario2")
+	     .setPosition(93,250)
+	     .setSize(185,26)
+	     .setFocus(true)
+	     .setFont(font)
+	     .setColor(255)
+	     .setColorBackground(color(255,255,255,1))
+	     .setColorActive(color(255,255,255,1)) 
+	     .removeBehavior() 
+	     .setLabelVisible(false)
+	     .setCaptionLabel("")
+	     ;
+		
+		 cp5.addTextfield("nombre")
+	     .setPosition(93,250)
+	     .setSize(185,26)
+	     .setFocus(true)
+	     .setFont(font)
+	     .setColor(255)
+	     .setColorBackground(color(255,255,255,1))
+	     .setColorActive(color(255,255,255,1)) 
+	     .removeBehavior() 
+	     .setLabelVisible(false)
+	     .setCaptionLabel("")
+	     ;
+		 
+		 cp5.addTextfield("email")
+	     .setPosition(93,250)
+	     .setSize(185,26)
+	     .setFocus(true)
+	     .setFont(font)
+	     .setColor(255)
+	     .setColorBackground(color(255,255,255,1))
+	     .setColorActive(color(255,255,255,1)) 
+	     .removeBehavior() 
+	     .setLabelVisible(false)
+	     .setCaptionLabel("")
+	     ;
+		 
+		 cp5.addTextfield("password2")
+	     .setPosition(93,250)
+	     .setSize(185,26)
+	     .setFocus(true)
+	     .setFont(font)
+	     .setColor(255)
+	     .setColorBackground(color(255,255,255,1))
+	     .setColorActive(color(255,255,255,1)) 
+	     .removeBehavior() 
+	     .setLabelVisible(false)
+	     .setCaptionLabel("")
+	     ;	
+	}
 	
+	
+	public void botones() {
+		  cp5.addButton("login")
+		     .setValue(0)
+		     .setPosition(145,198)
+		     .setSize(72,21)
+		     .setColorActive(color(255,255,255,1)) 
+		     .setColorBackground(color(255,255,255,1))
+		     .setColorForeground(color(0,0,0,30))
+		     .setCaptionLabel("") 
+		     ;
+		
+	}
 }
